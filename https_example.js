1faf06e9661902a0820db53dc25161978d41db81
lpenstone <laura.penstone@queensu.ca>
2017-07-02 00:18:17 +0000

    Added call to response from example.com

--- https_example.js
+++ https_example.js
@@ -14,6 +14,14 @@ var callback = function() {
 
 console.log("I'm about to make the request!");
 
-https.request(options, callback).end();
 
 console.log("I've made the request!");
+
+
+// called by https when the request is made.
+var callback = function(response) {
+  console.log('In response handler callback!');
+  console.log('Response: ', response);
+}
+
+https.request(options, callback).end();
